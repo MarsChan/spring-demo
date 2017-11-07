15c70508085e7c1d01a202c3f49fb221579b10b0
marscheng <marschenghz@gmail.com>
2017-11-07 15:51:48 +0800

    修改activiti5.16后的生成活动节点标记流程图的写法,增加注释

--- src/main/java/pers/marscheng/spring/utils/ProcessInstanceDiagramCmd.java
+++ src/main/java/pers/marscheng/spring/utils/ProcessInstanceDiagramCmd.java
@@ -3,6 +3,7 @@ package pers.marscheng.spring.utils;
 import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.impl.cmd.GetBpmnModelCmd;
 import org.activiti.engine.impl.interceptor.Command;
 import org.activiti.engine.impl.interceptor.CommandContext;
@@ -10,6 +11,7 @@ import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
 import org.activiti.engine.impl.persistence.entity.ExecutionEntityManager;
 
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * activiti命令执行类
@@ -27,6 +29,8 @@ public class ProcessInstanceDiagramCmd implements Command<InputStream> {
     @Override
     public InputStream execute(CommandContext commandContext) {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+
         ExecutionEntityManager executionEntityManager = commandContext
                 .getExecutionEntityManager();
         ExecutionEntity executionEntity = executionEntityManager
@@ -36,9 +40,12 @@ public class ProcessInstanceDiagramCmd implements Command<InputStream> {
         GetBpmnModelCmd getBpmnModelCmd = new GetBpmnModelCmd(
                 processDefinitionId);
         BpmnModel bpmnModel = getBpmnModelCmd.execute(commandContext);
+        //当前运行到的节点名称
+        List<String> activityIds = runtimeService.getActiveActivityIds(processInstanceId);
 
+        //activiti 5.16后改成这样,如果想要修改生成流程图的逻辑还可以继承DefaultProcessDiagramGenerator,覆盖里面的方法
         InputStream is = processEngine.getProcessEngineConfiguration().getProcessDiagramGenerator
-                ().generatePngDiagram(bpmnModel);
+                ().generateDiagram(bpmnModel,"png",activityIds);
 
         return is;
     }
